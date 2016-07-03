fb3beddc4c0233842b5d4966c8307bcd76102cfe
YXM <465184874@qq.com>
2016-07-03 18:02:41 +0800

    Update most-wanted-letter.py

--- home/most-wanted-letter.py
+++ home/most-wanted-letter.py
@@ -25,12 +25,12 @@ def most_letter(text):
     
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert checkio("Hello World!") == "l", "Hello test"
-    assert checkio("How do you do?") == "o", "O is most wanted"
-    assert checkio("One") == "e", "All letter only once."
-    assert checkio("Oops!") == "o", "Don't forget about lower case."
-    assert checkio("AAaooo!!!!") == "a", "Only letters."
-    assert checkio("abe") == "a", "The First."
+    assert most_letter("Hello World!") == "l", "Hello test"
+    assert most_letter("How do you do?") == "o", "O is most wanted"
+    assert most_letter("One") == "e", "All letter only once."
+    assert most_letter("Oops!") == "o", "Don't forget about lower case."
+    assert most_letter("AAaooo!!!!") == "a", "Only letters."
+    assert most_letter("abe") == "a", "The First."
     print("Start the long test")
-    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    assert most_letter("a" * 9000 + "b" * 1000) == "a", "Long."
     print("The local tests are done.")
