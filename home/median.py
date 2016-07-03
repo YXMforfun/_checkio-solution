afcb5723dee28d60876c067da7e087883f8c97a1
YXM <465184874@qq.com>
2016-07-03 18:01:18 +0800

    Update median.py

--- home/median.py
+++ home/median.py
@@ -21,10 +21,10 @@
 
 if __name__ == '__main__':
 
-    assert checkio([1, 2, 3, 4, 5]) == 3, "Sorted list"
-    assert checkio([3, 1, 2, 5, 3]) == 3, "Not sorted list"
-    assert checkio([1, 300, 2, 200, 1]) == 2, "It's not an average"
-    assert checkio([3, 6, 20, 99, 10, 15]) == 12.5, "Even length"
+    assert median([1, 2, 3, 4, 5]) == 3, "Sorted list"
+    assert median([3, 1, 2, 5, 3]) == 3, "Not sorted list"
+    assert median([1, 300, 2, 200, 1]) == 2, "It's not an average"
+    assert median([3, 6, 20, 99, 10, 15]) == 12.5, "Even length"
     print("Start the long test")
-    assert checkio(list(range(1000000))) == 499999.5, "Long."
+    assert median(list(range(1000000))) == 499999.5, "Long."
     print("The local tests are done.")
