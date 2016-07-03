584100ce78a0a6beccb3ed8582f3aaf6117bea2e
YXM <465184874@qq.com>
2016-07-03 18:03:39 +0800

    Update non-unique-elements.py

--- home/non-unique-elements.py
+++ home/non-unique-elements.py
@@ -16,8 +16,8 @@ def non_unique(data):
 
 if __name__ == "__main__":
     #These "asserts" using only for self-checking and not necessary for auto-testing
-     assert isinstance(checkio([1]), list), "The result must be a list"
-     assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
-     assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
-     assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
-     assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
+     assert isinstance(non_unique([1]), list), "The result must be a list"
+     assert non_unique([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+     assert non_unique([1, 2, 3, 4, 5]) == [], "2nd example"
+     assert non_unique([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+     assert non_unique([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
